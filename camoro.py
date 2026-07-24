205f2b3e60d200351a5bc134b50774535d5861ac
CamarO-Pro <fggyeyyyejchsysyfgd@gmail.com>
2026-07-24 21:39:29 +0200

    Delete camoro.py

--- camoro.py
+++
@@ -1,9 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-# Camoro launcher
-set -e
-cd "$(dirname "$0")"
-if command -v python3 >/dev/null 2>&1; then
-  python3 camoro.py "$@"
-else
-  python camoro.py "$@"
-fi
